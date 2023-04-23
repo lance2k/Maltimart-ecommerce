a548636ab3a77ab6d3df7ea068ced3474c04fde9
unknown <mhoneylance@gmail.com>
2023-04-23 19:08:13 +0800

    Finish maltimart ecommerce part 1

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,107 @@
+import React, { useRef, useEffect } from "react";
+import "./header.css";
+import logo from "../../assets/images/eco-logo.png";
+import { Container, Row } from "reactstrap";
+import { NavLink } from "react-router-dom";
+import userIcon from "../../assets/images/user-icon.png";
+import { motion } from "framer-motion";
+
+const nav__links = [
+	{
+		path: "home",
+		display: "Home",
+	},
+	{
+		path: "shop",
+		display: "Shop",
+	},
+	{
+		path: "cart",
+		display: "Cart",
+	},
+];
+
+const Header = () => {
+	const headerRef = useRef(null);
+    const menuRef = useRef(null);
+
+	const stickyHeaderFunc = () => {
+		window.addEventListener("scroll", () => {
+			if (
+				document.body.scrollTop > 80 ||
+				document.documentElement.scrollTop > 80
+			) {
+				headerRef.current.classList.add("sticky__header");
+			} else {
+				headerRef.current.classList.remove("sticky__header");
+			}
+		});
+	};
+
+	useEffect(() => {
+		stickyHeaderFunc();
+
+		return () => window.removeEventListener("scroll", stickyHeaderFunc);
+	});
+
+    const menuToggle = () => menuRef.current.classList.toggle('active__menu')
+
+	return (
+		<header className="header" ref={headerRef}>
+			<Container>
+				<Row>
+					<div className="nav__wrapper">
+						<div className="logo">
+							<img src={logo} alt="logo" />
+							<div>
+								<h1>Eductionery</h1>
+							</div>
+						</div>
+						<div className="navigation" ref={menuRef} onClick={menuToggle}>
+							<ul className="menu">
+								{nav__links.map((item, index) => (
+									<li className="nav__item" key={index}>
+										<NavLink
+											to={item.path}
+											className={(navClass) =>
+												navClass.isActive
+													? "nav__active"
+													: ""
+											}
+										>
+											{item.display}
+										</NavLink>
+									</li>
+								))}
+							</ul>
+						</div>
+						<div className="nav__icons">
+							<span className="fav__icon">
+								<i className="ri-heart-line"></i>
+								<span className="badge">1</span>
+							</span>
+							<span className="cart__icon">
+								<i className="ri-shopping-bag-line"></i>
+								<span className="badge">1</span>
+							</span>
+							<span>
+								<motion.img
+									whileTap={{ scale: 1.2 }}
+									src={userIcon}
+									alt=""
+								/>
+							</span>
+                            <div className="mobile__menu">
+                                <span onClick={menuToggle}>
+                                    <i className="ri-menu-line"></i>
+                                </span>
+						    </div>
+						</div>
+					</div>
+				</Row>
+			</Container>
+		</header>
+	);
+};
+
+export default Header;
